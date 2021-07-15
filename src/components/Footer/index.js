7c72cad382ac1d5808d9513a55dbc93ed5fbeba9
DUONG <c-dng>
2021-07-15 21:50:21 +0200

    footer test

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dropdown, Icon, Menu } from 'semantic-ui-react';
+import './style.scss';
+
+const Footer = () => (
+  <footer className="footer">
+  <p className="legal-notice">Mentions légales</p>
+  <p className="team">L'équipe</p>
+  <p className="contact-us">Contactez-nous</p>
+</footer>
+);
+
+export default Footer;
