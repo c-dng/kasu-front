15faa9577f3b6c16ea9aecf3d8bcd6a66017b282
oyatrides <oyatrides@gmail.com>
2021-07-16 18:47:39 +0200

    starting routes with the login page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,18 +5,31 @@ import Nav from 'src/components/Nav/';
 import Home from 'src/components/Home';
 import Footer from 'src/components/Footer';
 import HomeSearchBar from 'src/components/HomeSearchBar';
+// import Login from 'src/components/Login';
 
 // == Import
 
 import './styles.scss';
+import { Route, Switch } from 'react-router-dom';
 
 // == Composant
 const App = () => (
   <div className="app theme1">
+
     <Nav />
-    <HomeSearchBar />
-    <Home />
+
+    <Switch>
+      <Route path="/" exact>
+        <HomeSearchBar />
+        <Home />
+      </Route>
+      <Route path="/login" exact>
+        {/* <Login /> */}
+      </Route>
+    </Switch>
+
     <Footer />
+
   </div>
 );
 
