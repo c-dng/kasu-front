c5dcbf639cc8e4598a76c5d8996e18a5a8f501bd
oyatrides <oyatrides@gmail.com>
2021-07-16 18:38:59 +0200

    adding homeSearchbar, and repairing dev branch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,9 @@
 // == Import npm
 import React from 'react';
-import Nav from '/src/components/Nav';
-import Home from '/src/components/Home';
-import Footer from '/src/components/Footer';
+import Nav from 'src/components/Nav/';
+import Home from 'src/components/Home';
+import Footer from 'src/components/Footer';
+import HomeSearchBar from 'src/components/HomeSearchBar';
 // == Import
 
 import './styles.scss';
@@ -11,6 +12,7 @@ import './styles.scss';
 const App = () => (
   <div className="app theme1">
     <Nav />
+    <HomeSearchBar />
     <Home />
     <Footer />
   </div>
