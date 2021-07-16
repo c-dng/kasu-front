64a391180f0763f71afea88db21f1ca447c3b0f6
Mathieu Gauhtier <mathieugauthier1605@gmail.com>
2021-07-16 12:55:48 +0200

    Searchbar component added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 // == Import npm
 import React from 'react';
 import Nav from '/src/components/Nav';
+import SearchbarHome from '/src/components/SearchbarHome';
 import Footer from '/src/components/Footer';
 // == Import
 
@@ -10,6 +11,7 @@ import './styles.scss';
 const App = () => (
   <div className="app">
     <Nav />
+    <SearchbarHome/ >
     <Footer />
   </div>
 );
