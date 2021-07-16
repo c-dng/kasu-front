03e67cc092f61f17e7be1cb2c0be7da70e412584
oyatrides <oyatrides@gmail.com>
2021-07-16 15:44:11 +0200

    nav with double dropdown

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,59 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Dropdown, Icon, Menu } from 'semantic-ui-react';
+import { Dropdown, Icon, Menu, Segment } from 'semantic-ui-react';
 import './style.scss';
 import logo from './logo.png';
 
 const Nav = () => (
   <div className="nav">
-    <Menu icon="labeled" fluid compact>
-      <Menu.Item
-        name="Logo"
-      >
-        <img id="nav-logo" src={logo} alt="logo" />
-      </Menu.Item>
-      <Menu.Item
-        name="gamepad"
-      >
-        <Icon name="gamepad" />
-        Games
-      </Menu.Item>
-
-      <Menu.Item
-        name="avatar"
-      >
-        Avatar
-      </Menu.Item>
-
-      <Dropdown item icon="wrench" simple>
-        <Dropdown.Menu>
-          <Dropdown.Item>
-            <span className="text">Mon compte</span>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <span className="text">Mes collections</span>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Icon name="dropdown" />
-            <span className="text">Thèmes</span>
-
-            <Dropdown.Menu>
-              <Dropdown.Item>Thème 1</Dropdown.Item>
-              <Dropdown.Item>Thème 2</Dropdown.Item>
-              <Dropdown.Item>Thème 3</Dropdown.Item>
-              <Dropdown.Item>Thème 4</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown.Item>
-
-        </Dropdown.Menu>
-      </Dropdown>
-      <Menu.Item
-        name="Logout"
-
-      >
-        Logout
-      </Menu.Item>
-    </Menu>
+    <Segment inverted>
+      <Menu inverted icon fluid widths={5} borderless fixed="top">
+        <Menu.Item name="Logo">
+          <img id="nav-logo" src={logo} alt="logo" />
+        </Menu.Item>
+        <Menu.Item name="gamepad">
+          <Icon name="gamepad" />
+        </Menu.Item>
+        <Menu.Item name="avatar">
+          <Icon name="image" />
+        </Menu.Item>
+        <Dropdown icon="image" pointing className="link item">
+          <Dropdown.Menu direction="left">
+            <Dropdown.Header>Gestion de profil</Dropdown.Header>
+            <Dropdown.Item>Mon Compte</Dropdown.Item>
+            <Dropdown.Item>Ma Collection</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <Dropdown text="Mon thème">
+                <Dropdown.Menu>
+                  <Dropdown.Item>Thème 1</Dropdown.Item>
+                  <Dropdown.Item>Thème 2</Dropdown.Item>
+                  <Dropdown.Item>Thème 3</Dropdown.Item>
+                  <Dropdown.Item>Thème 4</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Menu.Item name="logout">
+          <Icon name="logout" />
+        </Menu.Item>
+      </Menu>
+    </Segment>
   </div>
 );
 
