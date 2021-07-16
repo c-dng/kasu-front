4b2a3f35612f55567b8dac3fd704efb807accc53
DUONG <c-dng>
2021-07-16 15:12:10 +0200

    bug button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,33 +5,31 @@ import './style.scss';
 const Home = () => (
   <div className="home">
       <Card.Group>
-    <Card>
-      <Card.Content>
+        <Card>
+          <Card.Content>
+            <Card.Description>
+              <strong>Kasu</strong>, qui veut dire "prêter" en japonais, est une plateforme offrant la possibilité aux passionnés de s'échanger mutuellement et temporairement leurs mangas.
+              <br></br>
+              Cherchez les collections disponibles près de chez vous!
+              <br></br>
+              Entrez en contact et définissez ensemble vos conditions d’échange.
+              <br></br>
+              Bonne lecture!
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+              <Form.Button >
+                Se connecter
+              </Form.Button>
+              <Form.Button >
+                S'inscrire
+              </Form.Button>
 
-        <Card.Description>
-          <strong>Kasu</strong>, qui veut dire "prêter" en japonais, estune plateforme offrant la possibilité aux passionnés de s'échanger mutuellement et temporairement leurs mangas.
-          <br></br>
-          Cherchez les collections disponibles près de chez vous!
-          <br></br>
-          Entrez en contact et définissez ensemble vos conditions d’échange.
-          <br></br>
-          Bonne lecture!
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Form.Button>
-            Se connecter
-          </Form.Button>
-          <Form.Button>
-            S'inscrire
-          </Form.Button>
-
-        </div>
-      </Card.Content>
-    </Card>
-    
-  </Card.Group>
+            </div>
+          </Card.Content>
+        </Card>
+      </Card.Group>
   </div>
 );
 
