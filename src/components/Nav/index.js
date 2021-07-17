66a7f46104bb1374b4bb79fa5e20e75e87e7e181
oyatrides <oyatrides@gmail.com>
2021-07-17 11:44:14 +0200

    nav style adjustment

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Dropdown, Icon, Image, Menu, Segment,
+  Dropdown, Image, Menu, Segment,
 } from 'semantic-ui-react';
 import './style.scss';
+import { Link } from 'react-router-dom';
 import logo from './logo.png';
 import chatlogo from './chatlogo.png';
-import dropdownlogo from './dropdownlogo.png';
 import logoutlogo from './logoutlogo.png';
-import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <div className="nav">
     <Segment className="nav-segment">
-      <Menu className="nav-menu" icon fluid widths={5} borderless fixed="top">
+      <Menu className="nav-menu" icon fluid widths={4} borderless fixed="top">
         <Menu.Item className="nav-logoContainer" name="Logo">
-          <Image as={Link} to="/" exact className="nav-logo" src={logo} alt="logo" />
+          <Link to="/" exact={+true} className="nav-logoLinkContainer">
+            <Image className="nav-logo" src={logo} alt="logo" />
+          </Link>
         </Menu.Item>
         <Menu.Item>
           <Image circular className="navbuttons" id="temporary-avatar" src={chatlogo} alt="avatar-logo" size="mini" />
           <Dropdown floating icon="dropdown">
             <Dropdown.Menu>
               <Dropdown.Header>Gestion de profil</Dropdown.Header>
-              <Dropdown.Item as={Link} to="/profil/:id" exact>Mon Compte</Dropdown.Item>
-              <Dropdown.Item as={Link} to="/profil/mes-infos" exact>Mes Infos Personnelles</Dropdown.Item>
-              <Dropdown.Item as={Link} to="/profil/collection" exact>Ma Collection</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/profil/:id" exact={+true}>Mon Compte</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/profil/mes-infos" exact={+true}>Mes Infos Personnelles</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/profil/collection" exact={+true}>Ma Collection</Dropdown.Item>
               <Dropdown.Divider />
               <Dropdown.Item>
                 <Dropdown text="Mon thème">
@@ -40,10 +41,10 @@ const Nav = () => (
           </Dropdown>
         </Menu.Item>
         <Menu.Item name="chat">
-          <Image as={Link} to="/conversations" exact circular className="navbuttons" src={chatlogo} alt="logo" size="mini" />
+          <Image as={Link} to="/conversations" exact={+true} className="navbuttons" src={chatlogo} alt="logo" size="mini" />
         </Menu.Item>
         <Menu.Item name="logout">
-          <Image circular className="navbuttons" src={logoutlogo} alt="logo" size="mini" />
+          <Image className="navbuttons" src={logoutlogo} alt="logo" size="mini" />
         </Menu.Item>
       </Menu>
     </Segment>
