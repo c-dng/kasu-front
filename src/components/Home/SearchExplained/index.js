935e73073265ad4de86b5e7f4da7d35306912e75
oyatrides <oyatrides@gmail.com>
2021-07-19 10:18:22 +0200

    trying design improvements on SearchExplained component

--- src/components/Home/SearchExplained/index.js
+++ src/components/Home/SearchExplained/index.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import { Card, Divider, Image } from 'semantic-ui-react';
+import { Divider } from 'semantic-ui-react';
 import SearchBar from '../../SearchBar';
-import homeCardLogo from '../home-card-logo.jpg';
 
 const SearchExplained = () => (
   <div className="searchExplained">
-    <Card className="home-card" centered>
-      <Card.Content>
-        <Card.Header>Découvrez les collections près de chez vous</Card.Header>
-        <Divider />
-        <Card.Description>
-          Cherchez les collections disponibles près de chez vous, entrez en contact
-          et définissez ensemble vos conditions d’échange.
-          Pour commencer, tapez le nom de votre ville dans la barre de recherche.
-          Bonne lecture !
-        </Card.Description>
-      </Card.Content>
-      <Card.Content className="home-cardImageContent">
-        {/* <Image className="home-cardImage" src={homeCardLogo} /> */}
-        <SearchBar />
-      </Card.Content>
-    </Card>
+    <h2 className="searchExplained-title">Découvrez les collections près de chez vous</h2>
+    <Divider />
+    <p className="searchExplained-description">
+      Cherchez les collections disponibles près de chez vous, entrez en contact
+      et définissez ensemble vos conditions d’échange.
+      Pour commencer, tapez le nom de votre ville dans la barre de recherche.
+      Bonne lecture !
+    </p>
+    <SearchBar />
   </div>
 );
 
