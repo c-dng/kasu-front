2aa21856315cc527f319540fe9000cae6886e65a
oyatrides <oyatrides@gmail.com>
2021-07-19 10:48:54 +0200

    no Divider

--- src/components/Home/SearchExplained/index.js
+++ src/components/Home/SearchExplained/index.js
@@ -5,7 +5,7 @@ import SearchBar from '../../SearchBar';
 const SearchExplained = () => (
   <div className="searchExplained">
     <h2 className="searchExplained-title">Découvrez les collections près de chez vous</h2>
-    <Divider />
+    {/* <Divider /> */}
     <p className="searchExplained-description">
       Cherchez les collections disponibles près de chez vous, entrez en contact
       et définissez ensemble vos conditions d’échange.
