2058935eb0847fda42e485dc6358096f968f3c47
DUONG <c-dng>
2021-07-19 22:31:28 +0200

    ManageMyCollection finished : but I just didn't manage to position the desktop dropdown at the same length as the 'add to my collection' button

--- src/components/ManageMyCollection/index.js
+++ src/components/ManageMyCollection/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header, Divider, Icon, Image, Segment, Dropdown, Button } from 'semantic-ui-react'
-
+import SearchBar from 'src/components/SearchBar';
 import './style.scss';
 
 const options = [
@@ -18,80 +18,77 @@ const options = [
 const ManageMyCollection = () => (
   <div className="manageMyCollection">
 
-    <h1>Gestion des collections</h1>
-    <Header as='h4' attached='centered'>
-      Résultat de votre recherche
-    </Header>
+    <h2>Gestion des collections</h2>
+      <SearchBar />
 
-    <Header className="titleNameManga " as='h3'>
-    Seven Deadly Sins
-    </Header>
-    <Image className="Manga-image"
+    <div className="subtitle">
+      <Header as='h4' attached>
+      Résultat de votre recherche
+      </Header>
+    </div>
+
+    <div className="titleNameManga">
+      <Header as='h3'>
+      Seven Deadly Sins
+      </Header>
+    </div>
+      <Image className="manga-image"
         src="https://images-na.ssl-images-amazon.com/images/I/91GSy4frSdL.jpg"
         size="small"
         centered
     />
 
-    <div className="LineVolumeToAdd">
-      <Dropdown className="dropdownChooseManga" placeholder='Tome(s) à ajouter' size="5" fluid multiple selection options={options} />
-      <div className="ButtonAddToMyCollection">
-      <Button className="add-to-my-collection" color="red">
-        Ajouter à ma collection
-      </Button>
-      </div>
+    <div className="lineVolumeToAdd">
+        <Dropdown className="dropdownChooseManga" placeholder='Tome(s) à ajouter' size="5" fluid multiple selection options={options} />
+          <Button className="add-to-my-collection" color="red">
+            Ajouter à ma collection
+          </Button>
     </div>
 
-    <Header className="myCollection" as='h2'>
-    <Icon name='settings' />
-    <Header.Content>
-        Ma collection
-      <Header.Subheader>Gérer la visibilité, éditer, supprimer</Header.Subheader>
-    </Header.Content>
-    </Header>
+    <div className="myCollection">
+      <Header as='h2'>
+        <Icon name='settings' />
+        <Header.Content>
+            Ma collection
+          <Header.Subheader>
+            Gérer la visibilité, l'édition, ou la suppression
+          </Header.Subheader>
+        </Header.Content>
+      </Header>
+    </div>
 
-    <Divider></Divider>
+      <Divider></Divider>
 
     <div className="globalCardManga">
-    
-      <div className="myMangas">
-    <Header className="titleNameManga " as='h3'>
-        Attaque des Titans
-    </Header>
+        <div className="header-title-manga-owner">
+          <Header as='h3'>
+            Attaque des Titans
+          </Header>
+        </div>
 
-    <Image className="Manga-image"
-        src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/313968/313968._SX1280_QL80_TTD_.jpg"
-        size="small"
-        centered
-    />
+      <Image className="Manga-image"
+          src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/313968/313968._SX1280_QL80_TTD_.jpg"
+          size="small"
+          centered
+      />
 
-    <div className="tree-buttons">
-    <Segment>
-      <Dropdown placeholder='Choisir le(s) tome(s)' size="5" fluid multiple selection options={options} />
-      <Button color='black'>
-          Disponibilité
-      </Button>
-      <Button color='black'>
-          Éditer
-      </Button>
-      <Button color='red'>
-          Supprimer
-      </Button>
-      </Segment>
+      <div className="tree-buttons">
+      <Segment>
+        <Dropdown
+          placeholder='Choisir le(s) tome(s)' size="5" fluid multiple selection options={options} />
+        <Button color='black'>
+            Disponibilité
+        </Button>
+        <Button color='black'>
+            Éditer
+        </Button>
+        <Button color='red'>
+            Supprimer
+        </Button>
+        </Segment>
+      </div>
     </div>
-
-    
-    </div>
-
-
-    
-
-
-
-
-
 </div>
-</div>
-
 
 );
 
