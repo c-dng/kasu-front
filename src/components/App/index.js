118a3e67f4e73b4328e53d94748c85bba5c49fca
oyatrides <oyatrides@gmail.com>
2021-07-21 20:34:17 +0200

    removed the legalplace link

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -22,7 +22,6 @@ import { Route, Switch } from 'react-router-dom';
 import NoAccountBox from '../NoAccountBox';
 import AlreadyAccountBox from '../AlreadyAccountBox';
 
-
 // == Composant
 const App = ({ theme, autoLogin }) => {
   const token = localStorage.getItem('token');
