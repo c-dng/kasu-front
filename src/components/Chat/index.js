c03cdc1e649bd3ca16c00263d8354f316506255e
Laetitia Deschamps <name>
2021-08-03 14:24:17 +0200

    design chat

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,12 +3,24 @@ import React from 'react';
 import Messages from '../../containers/Chat/Messages';
 import ChatField from '../../containers/Chat/ChatField';
 
-const Chat = ({ }) => (
-  <div className="chat">
-    <Messages />
-    <ChatField />
-  </div>
-);
+const Chat = ({ picture, users, userId }) => {
+
+  users = users.filter(user=> {
+    return user.id !== userId
+  })
+  let otherUserPicture = users[0].picture;
+
+  return (
+    <div className="chat">
+      <img src={`https://api.multiavatar.com/${picture}.png`} className="picture"/>
+     
+        <Messages />
+        <ChatField />
+     
+      <img src={`https://api.multiavatar.com/${otherUserPicture}.png`} className="picture"/>
+    </div>
+  );
+} 
 
 Chat.propTypes = {
 
