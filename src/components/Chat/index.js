813d567a853c9cc984aed0599526b08fad57e1c8
Laetitia Deschamps <name>
2021-08-03 15:25:09 +0200

    comments

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -4,7 +4,7 @@ import Messages from '../../containers/Chat/Messages';
 import ChatField from '../../containers/Chat/ChatField';
 
 const Chat = ({ picture, users, userId }) => {
-
+// We filter the chat users to get the one that is not the logged in user 
   users = users.filter(user=> {
     return user.id !== userId
   })
